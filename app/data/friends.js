36ecbf15b992987946a1f6a7b77be21833d58698
shrill <cheryl.olonan@gmail.com>
2019-05-26 12:53:41 -0700

    test files

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+	
+	{
+		name: "Thor Odinson",
+		photo: "https://img.cinemablend.com/filter:scale/quill/2/f/2/a/d/e/2f2adeb636270a71d6257d69093a1d34a6685e66.jpg?mw=600",
+		scores: [
+			"5",
+			"3",
+			"4",
+			"3",
+			"2",
+			"5",
+			"1",
+			"4",
+			"3",
+			"1"
+		]
+	},
+	{
+		name: "Steve Rogers",
+		photo: "https://boygeniusreport.files.wordpress.com/2014/04/captain-america.jpg?quality=98&strip=all",
+		scores: [
+			"3",
+			"1",
+			"5",
+			"4",
+			"3",
+			"5",
+			"1",
+			"1",
+			"5",
+			"3"
+		]
+	},
+	{
+		name: "Arthur Curry",
+		photo: "https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Finsertcoin%2Ffiles%2F2018%2F12%2Faqua2.jpg",
+		score: [
+			"3",
+			"1",
+			"4",
+			"2",
+			"4",
+			"3",
+			"5",
+			"6",
+			"5",
+			"4"		
+        ]
+    }
+];
+
+module.exports = friends;
